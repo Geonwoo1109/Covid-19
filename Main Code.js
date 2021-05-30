3825a1fc5db0290a3f23a857b1be74d2c8f76d16
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2021-05-30 23:52:15 +0900

    Update Main Code.js

--- Main Code.js
+++ Main Code.js
@@ -10,7 +10,8 @@ const Site_1 = "https://api.corona-19.kr/korea/?serviceKey=";
 const Site_2 = "https://api.corona-19.kr/korea/country/new/?serviceKey=";
 
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
-  if ((msg == ".자가진단" && sender == "방장봇") || msg == ".코로나") {
+  if ((msg == ".자가진단" && sender == "방장봇") || msg == ".코로나") { //매일아침 방장봇이 명령어를 입력할 수 있도록 해주세요
+    //자동으로 아침 몇시에 하는것도 있을라나?
     try {
       var Main_Info = JSON.stringify(JSON.parse(org.jsoup.Jsoup.connect(Site_1 + Key).ignoreContentType(true).get().text()),null,4);
       replier.reply(Main_Info);
